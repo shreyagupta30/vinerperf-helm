a88f12c989350b58b300a099c2d4efed875c1140
shreyagupta30 <shreyagupta3011@gmail.com>
2021-08-07 03:17:47 +0530

    add python scipt

--- pyscript/main.py
+++ pyscript/main.py
@@ -3,6 +3,7 @@ import subprocess
 import yaml
 from kubernetes import client, config
 
+
 def check_system_installations():
     try:
         subprocess.Popen(f"helm version",shell=True,stdout=subprocess.PIPE,).stdout.read()
@@ -11,26 +12,7 @@ def check_system_installations():
 
     except subprocess.CalledProcessError as e:
         print (e.output)
-        install()
 
-def install():
-    print("Installing Kubectl")
-    subprocess.run('curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"', shell=True)
-    subprocess.run('curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"', shell=True)
-    subprocess.run('echo "$(<kubectl.sha256) kubectl" | sha256sum --check', shell=True)
-    subprocess.run("sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl", shell=True)
-    print("Installing minikube")
-    subprocess.run("curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64", shell=True)
-    subprocess.run("sudo install minikube-linux-amd64 /usr/local/bin/minikube")
-    print("Installing Helm")
-    subprocess.run("curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3", shell=True)
-    subprocess.run("chmod 700 get_helm.sh", shell=True)
-    subprocess.run("./get_helm.sh", shell=True)
-
-def parse_kubernetes_config(file_path):
-    config.load_kube_config(config_file = file_path)
-    v1 = client.CoreV1Api()
-    return v1
 
 def parse_helm_chart(helm_path):
     values_file = os.path.join(helm_path,"Chart.yaml")
@@ -40,22 +22,30 @@ def parse_helm_chart(helm_path):
 
     return name_of_chart
 
-
 def main():
+
     check_system_installations()
 
     helm_location = input("Enter the location of helm chart:  ")
-    config_k8_file = input("Enter the location of Kubernetes configuration file to be used(or press enter to pickup system defaults: ") or "~/.kube/config"
-    
-    v1 = parse_kubernetes_config(config_k8_file)
+        
     name = parse_helm_chart(helm_location)
+    subprocess.Popen(f"helm install {name} {helm_location}",shell=True,stdout=subprocess.PIPE,).communicate()
 
-    subprocess.Popen(f"kubectl create namespace {name}",shell=True,stdout=subprocess.PIPE,).stdout.read()
-    subprocess.Popen(f"helm install -f {os.path.join(helm_location, 'values.yaml')} -n {name} {name} {helm_location}",shell=True,stdout=subprocess.PIPE,).communicate()
+    config.load_kube_config()
 
-    pod_list = v1.list_namespaced_pod(f"{name}")
-    for pod in pod_list.items:
-        print("%s\t%s\t%s\t%s\t%s" % (pod.metadata.name, pod.status.phase, pod.status.pod_i_ps, pod.status.host_ip, pod.status.conditions))
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" %
+        (i.status.pod_ip, i.metadata.namespace, i.metadata.name ))
+    
+    #status of helm charts
+    print("\nStatus of helm charts\n")
+    subprocess.run("helm list", shell =True)
+
+    print("\nListing services and its status\n")
+    subprocess.run("kubectl get service", shell=True)
 
 if __name__ == "__main__":
     main()
