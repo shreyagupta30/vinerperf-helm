dc6b9b1d0ef46c22736cb7cd093fc1b07bba0114
shreyagupta30 <shreyagupta3011@gmail.com>
2021-08-13 11:05:02 +0530

    add python scipt

--- pyscript/main.py
+++ pyscript/main.py
@@ -1,8 +1,13 @@
 import os
 import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import json
 import yaml
-from kubernetes import client, config
+from rich.console import Console
+from rich.table import Table
+from nested_lookup import nested_lookup
 
+console = Console()
 
 def check_system_installations():
     try:
@@ -31,21 +36,36 @@ def main():
     name = parse_helm_chart(helm_location)
     subprocess.Popen(f"helm install {name} {helm_location}",shell=True,stdout=subprocess.PIPE,).communicate()
 
-    config.load_kube_config()
-
-    v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-        (i.status.pod_ip, i.metadata.namespace, i.metadata.name ))
-    
-    #status of helm charts
+    # status of helm charts
     print("\nStatus of helm charts\n")
     subprocess.run("helm list", shell =True)
+    print("--" * 70)
 
-    print("\nListing services and its status\n")
-    subprocess.run("kubectl get service", shell=True)
+    print("\nDEPLOYEMENT DETAILS\n")
+
+    pp = subprocess.Popen(f"kubectl get service -o json {name}", shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    output = pp.stdout.read()
+    string = output.decode().replace("'", '"')
+    _json = eval(string)
+
+    table = Table(show_header=True)
+
+    table.add_column("NAME")
+    table.add_column("Type")
+    table.add_column("CLUSTER-IP")
+    table.add_column("EXTERNAL-IP")
+    table.add_column("PORT(S)")
+
+    tt = f'{_json["spec"]["ports"][0]["port"]}:{_json["spec"]["ports"][0]["nodePort"]}/{_json["spec"]["ports"][0]["protocol"]}'
+
+    table.add_row(
+        f'{_json["metadata"]["name"]}',
+        f'{_json["spec"]["type"]}',
+        f'{_json["spec"]["clusterIP"]}',
+        f'{_json["status"]["loadBalancer"]}',
+        tt
+    )
+    console.print(table)
 
 if __name__ == "__main__":
     main()
